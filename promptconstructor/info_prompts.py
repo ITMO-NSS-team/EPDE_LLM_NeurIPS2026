06b53fef8ce89dc662831497fefcb1e0d05fb5e6
Gromwud <timur.bavshin@gmail.com>
2025-09-12 15:14:20 +0300

    prompts update

--- promptconstructor/info_prompts.py
+++ promptconstructor/info_prompts.py
@@ -1,23 +1,23 @@
 # дописать случаи когда на левой стороне не та производная
 
 prompt_complete_inf = {
-    'burg': {'full_form': "du/dt = F(t, x, u, du/dx)",
-             'dots_order': "t x u du/dt du/dx",
+    'burg': {'full_form': "du/dt = F(t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3)",
+             'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3',
              'left_deriv': 'du/dt',},
 
-    'burg_sindy': {'full_form': "du/dt = F(t, x, u, du/dx, d^2u/dt^2, d^2u/dx^2)",
-                   'dots_order': "t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2",
+    'burg_sindy': {'full_form': "du/dt = F(t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3)",
+                   'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3',
                    'left_deriv': 'du/dt',},
 
-    'kdv': {'full_form': "du/dt = F(t, x, u, du/dx, d^2u/dx^2, d^3u/dx^3)",
-            'dots_order': 't x u du/dt du/dx d^2u/dx^2 d^3u/dx^3',
+    'kdv': {'full_form': "du/dt = F(t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3)",
+            'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3',
             'left_deriv': 'du/dt', },
 
-    'kdv_sindy': {'full_form': "du/dt = F(t, x, u, du/dx, d^2u/dt^2, d^2u/dx^2, d^3u/dx^3)",
+    'kdv_sindy': {'full_form': "du/dt = F(t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3)",
                   'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3',
                   'left_deriv': 'du/dt', },
 
-    'wave': {'full_form': "d^2u/dt^2 = F(t, x, u, du/dt, du/dx, d^2u/dx^2)",
-             'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2',
+    'wave': {'full_form': "d^2u/dt^2 = F(t x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3)",
+             'dots_order': 't x u du/dt du/dx d^2u/dt^2 d^2u/dx^2 d^3u/dx^3',
              'left_deriv': 'd^2u/dt^2',},
 }
