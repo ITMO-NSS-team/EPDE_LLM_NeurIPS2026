91d43e044046e23d172c74d42be789fc82af9c1a
Gromwud <timur.bavshin@gmail.com>
2025-09-22 09:35:56 +0300

    schemas bugfix

--- epde_eq_parse/schemas.py
+++ epde_eq_parse/schemas.py
@@ -35,21 +35,21 @@ class BurgSch(object):
 class BurgSindySch(object):
     left_side = 'du/dx0'
     schema = frozenset({'du/dx0', 'u * du/dx1', 'd^2u/dx1^2', 'C'})
-    correct_params1 = {'du/dx0': 1.0, 'u * du/dx1': 1.0, 'd^2u/dx1^2': -0.1, 'C': 0.0}
-    correct_params2 = {'du/dx0': 10.0, 'u * du/dx1': 10.0, 'd^2u/dx1^2': -1.0, 'C': 0.0}
+    correct_params1 = {'du/dx0': -1.0, 'u * du/dx1': -1.0, 'd^2u/dx1^2': -0.1, 'C': 0.0}
+    correct_params2 = {'du/dx0': -10.0, 'u * du/dx1': -10.0, 'd^2u/dx1^2': -1.0, 'C': 0.0}
     correct_params3 = {'du/dx0': -1.0, 'u * du/dx1': -1.0, 'd^2u/dx1^2': 0.1, 'C': 0.0}
     correct_params4 = {'du/dx0': -10.0, 'u * du/dx1': -10.0, 'd^2u/dx1^2': 1.0, 'C': 0.0}
-    params = [correct_params1, correct_params2, correct_params3, correct_params4]
+    # correct_params5 = {'du/dx0': -1.0, 'u * du/dx1': 1.0, 'd^2u/dx1^2': 0.1, 'C': 0.0}
+    # correct_params6 = {'du/dx0': -10.0, 'u * du/dx1': 10.0, 'd^2u/dx1^2': 1.0, 'C': 0.0}
+    params = [correct_params1, correct_params2, correct_params3, correct_params4] #, correct_params5, correct_params6
 
 
 class KdvSindySch(object):
     left_side = 'du/dx0'
     schema = frozenset({'du/dx0', 'u * du/dx1', 'd^3u/dx1^3', 'C'})
-    correct_params1 = {'du/dx0': 1.0, 'u * du/dx1': 6.0, 'd^3u/dx1^3': 1.0, 'C': 0.0}
-    correct_params2 = {'du/dx0': 1/6, 'u * du/dx1': 1.0, 'd^3u/dx1^3': 1/6, 'C': 0.0}
-    correct_params3 = {'du/dx0': -1.0, 'u * du/dx1': -6.0, 'd^3u/dx1^3': -1.0, 'C': 0.0}
-    correct_params4 = {'du/dx0': -1/6, 'u * du/dx1': -1.0, 'd^3u/dx1^3': -1/6, 'C': 0.0}
-    params = [correct_params1, correct_params2, correct_params3, correct_params4]
+    correct_params1 = {'du/dx0': -1.0, 'u * du/dx1': -6.0, 'd^3u/dx1^3': -1.0, 'C': 0.0}
+    correct_params2 = {'du/dx0': -1/6, 'u * du/dx1': -1.0, 'd^3u/dx1^3': -1/6, 'C': 0.0}
+    params = [correct_params1, correct_params2]
 
 
 class KdvSch(object):
